1893938b6eca74f71228f295b229d7cc035045f8
Lorenzo Alberto <lorenzoalberto02@gmail.com>
2026-01-14 21:32:18 -0300

    :sparkles: feat: Estrutura base do frontend

---
+++ src/app/layout.tsx
@@ -0,0 +1,12 @@
+// componente que envolve todas as páginas dentro de uma rota (layout global)
+export default function RootLayout({
+    children,
+}: {
+    children: React.ReactNode;
+}) {
+    return (
+        <html lang="pt-BR">
+            <body>{children}</body>
+        </html>
+    );
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,4 @@
+// rota '/' do site (home)
+export default function Home() {
+    return <h1>Título</h1>;
+}
